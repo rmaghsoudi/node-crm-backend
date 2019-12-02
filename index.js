c813fff8629ab51791aa8e649996c9932ba58693
Romy Maghsoudi <romy_mag@outlook.com>
2019-12-02 13:52:42 -0500

    database setup

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import express from 'express';
 import routes from './src/routes/crmRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 3000;
+
+// mongoose connection
+// states that we're going to wait for a response from mongodb
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// body parser set up
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
